37d2a73475f21bb470a5ae726a07ca8ed598d316
nro04 <ronicolas04@gmail.com>
2021-12-03 14:54:15 -0500

    Middleware added

---
+++ app/Http/Middleware/JWT/JwtMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware\JWT;
+
+use Closure;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth as JWTAuth;
+
+class JwtMiddleware
+{
+
+    const EXCEPTIONS = [
+        "TokenInvalidException", "Token is Invalid",
+        "TokenExpiredException", "Token is Expired",
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        try {
+            JWTAuth::parseToken();
+        } catch (\Exception $e) {
+            return response()->json([
+                    'status' => self::EXCEPTIONS[$this->getClassName($e)],
+                    'code' => $e->getCode(),
+                ] ?? "Authentication Error, Token is not found.");
+        }
+
+        return $next($request);
+    }
+
+    public function getClassName($event)
+    {
+        //Transform to array where it found
+        $class = explode('\\', get_class($event));
+        //returns class name
+        return array_pop($class);
+    }
+}
